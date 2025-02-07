c5c30ac687610967d636ac50161158240b8eb6f0
Oh <osm723@gmail.com>
2025-02-07 15:06:26 +0900

    1. ip조회 진행중

--- src/main/java/com/shds/sma/manage/dto/ip/IpRequestDto.java
+++ src/main/java/com/shds/sma/manage/dto/ip/IpRequestDto.java
@@ -1,12 +1,31 @@
 package com.shds.sma.manage.dto.ip;
 
+import com.shds.sma.manage.types.IpType;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class IpRequestDto {
+
+    private String startIpAddr;
+
+    private String endIpAddr;
+
+    private String systemName;
+
+    private String memberName;
+
+    private String validity;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+
 }
