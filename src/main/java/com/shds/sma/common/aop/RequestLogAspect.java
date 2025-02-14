265a0d9624ddb8cad00952a2a30482100135885a
Oh <osm723@gmail.com>
2025-02-14 10:34:34 +0900

    1. aop 추가 (api로그, 배치로그)

---
+++ src/main/java/com/shds/sma/common/aop/RequestLogAspect.java
@@ -0,0 +1,26 @@
+package com.shds.sma.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class RequestLogAspect {
+
+    @Before("execution(* com.shds.sma.api.controller..*(..))")
+    public void addApiLog(JoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        log.info("======== API 요청 ======== {} : {}",signature.getDeclaringTypeName(), signature.getName());
+    }
+
+    @Before("execution(* com.shds.sma.batch.controller..*(..))")
+    public void addBatchLog(JoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        log.info("======== Batch 요청 ======== {} : {}",signature.getDeclaringTypeName(), signature.getName());
+    }
+}
