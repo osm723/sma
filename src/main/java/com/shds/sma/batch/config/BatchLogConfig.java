02ede3c704068638d630620cb9729f1df885c50f
Oh <osm723@gmail.com>
2025-02-26 21:34:07 +0900

    1. 스프링 배치로 10분마다 에러로그를 조회해서 알림 발송

--- src/main/java/com/shds/sma/batch/config/BatchLogConfig.java
+++ src/main/java/com/shds/sma/batch/config/BatchLogConfig.java
@@ -3,6 +3,7 @@ package com.shds.sma.batch.config;
 import com.shds.sma.alarm.service.AlarmService;
 import com.shds.sma.batch.listener.JobListener;
 import com.shds.sma.batch.writer.ErrorLogDailyWriter;
+import com.shds.sma.batch.writer.ErrorLogMinuteWriter;
 import com.shds.sma.log.entity.Log;
 import com.shds.sma.log.repository.LogRepository;
 import lombok.RequiredArgsConstructor;
@@ -37,6 +38,10 @@ public class BatchLogConfig {
 
     private final AlarmService alarmService;
 
+    private static final LocalDateTime LOCAL_DATE_TIME_MINUS_ONE_DAY = LocalDateTime.now().minusDays(1);
+    private static final LocalDateTime LOCAL_DATE_TIME_MINUS_TEN_MINUTE = LocalDateTime.now().minusMinutes(10);
+
+    // ===== Job : errorLogDailyJob 데이터 처리 =====
     /**
      * Reader
      * 역할: 데이터 소스(예: 데이터베이스, 파일 등)에서 데이터를 한 항목씩 읽어옵니다.
@@ -49,7 +54,7 @@ public class BatchLogConfig {
         RepositoryItemReader<Log> reader = new RepositoryItemReader<>();
         reader.setRepository(logRepository);
         reader.setMethodName("findByLogDateAfter");
-        reader.setArguments(Collections.singletonList(LocalDateTime.now().minusDays(1)));
+        reader.setArguments(Collections.singletonList(LOCAL_DATE_TIME_MINUS_ONE_DAY));
         Map<String, Sort.Direction> sorts = new HashMap<>();
         sorts.put("id", Sort.Direction.ASC);
         reader.setSort(sorts);
@@ -112,6 +117,86 @@ public class BatchLogConfig {
                 .reader(errorLogDailyReader(logRepository))
                 .processor(item -> item)
                 .writer(errorLogDailyWriter())
+                .build();
+    }
+
+
+    // ===== Job : errorLogMinuteJob 데이터 처리 =====
+    /**
+     * Reader
+     * 역할: 데이터 소스(예: 데이터베이스, 파일 등)에서 데이터를 한 항목씩 읽어옵니다.
+     * 설명: Chunk 기반 Step에서는 ItemReader가 각 청크의 첫 단계로, 데이터를 순차적으로 읽어와 다음 단계로 전달합니다.
+     *
+     * @return
+     */
+    @Bean
+    public RepositoryItemReader<Log> errorLogMinuteReader(LogRepository logRepository) {
+        RepositoryItemReader<Log> reader = new RepositoryItemReader<>();
+        reader.setRepository(logRepository);
+        reader.setMethodName("findByLogDateAfter");
+        reader.setArguments(Collections.singletonList(LOCAL_DATE_TIME_MINUS_TEN_MINUTE));
+        Map<String, Sort.Direction> sorts = new HashMap<>();
+        sorts.put("id", Sort.Direction.ASC);
+        reader.setSort(sorts);
+        reader.setPageSize(100);
+
+        return reader;
+    }
+
+    /**
+     * Processor
+     * 역할: 읽어온 데이터를 비즈니스 로직에 따라 변환하거나 검증합니다.
+     * 설명: ItemProcessor는 각 아이템에 대해 처리(예: 데이터 가공, 필터링)를 수행하며, 변환된 결과를 Writer로 전달합니다.
+     * 특징: 처리 결과가 null이면 해당 아이템은 Writer로 전달되지 않아 필터링 효과를 가질 수 있습니다.
+     *
+     * @return
+     */
+
+    /**
+     * Writer
+     * 역할: 처리된 데이터를 최종 목적지(예: 데이터베이스, 파일 등)에 기록합니다.
+     * 설명: Chunk 단위로 모인 처리된 아이템들을 한 번에 쓰는 역할을 하며, 데이터의 일괄 저장(또는 업데이트)을 수행합니다.
+     *
+     * @return
+     */
+    @Bean
+    public ErrorLogMinuteWriter errorLogMinuteWriter() {
+        return new ErrorLogMinuteWriter(alarmService);
+    }
+
+    /**
+     * Job
+     * 역할: 배치 처리의 전체 단위를 정의합니다.
+     * 설명: Job은 하나 이상의 Step으로 구성되며, 실행 순서(순차, 분기 등)와 전체 흐름을 관리합니다.
+     * 또한 JobExecutionListener를 통해 작업 시작/종료 시 추가 로직을 수행할 수 있습니다.
+     * @param listener
+     * @return
+     */
+    @Bean
+    public Job errorLogMinuteJob(JobListener listener) {
+        return new JobBuilder("errorLogMinuteJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(errorLogMinuteStep())
+                .end()
+                .build();
+    }
+
+    /**
+     * Step
+     * 역할: Job의 개별 단계로, 한 가지 특정 작업 단위를 수행합니다.
+     * 설명: Step은 보통 두 가지 방식으로 구현됩니다.
+     * - Chunk 기반 처리: 데이터를 일정 청크 단위로 읽고, 처리한 후, 결과를 쓰는 과정을 반복합니다.
+     * - Tasklet 기반 처리: 단일 작업을 수행하는 간단한 로직을 구현할 때 사용합니다.
+     * @return
+     */
+    @Bean
+    public Step errorLogMinuteStep() {
+        return new StepBuilder("errorLogMinuteStep", jobRepository)
+                .<Log,Log>chunk(100, platformTransactionManager)
+                .reader(errorLogMinuteReader(logRepository))
+                .processor(item -> item)
+                .writer(errorLogMinuteWriter())
                 .build();
     }
 
--- src/main/java/com/shds/sma/batch/scheduler/BatchScheduler.java
+++ src/main/java/com/shds/sma/batch/scheduler/BatchScheduler.java
@@ -34,12 +34,15 @@ public class BatchScheduler {
 
     private final Job errorLogDailyJob;
 
+    private final Job errorLogMinuteJob;
+
     public BatchScheduler(LogService logService, IpService ipService, CertService certService, JobLauncher jobLauncher,
                           @Qualifier("importPersonJob") Job importPersonJob,
                           @Qualifier("processOrderJob") Job processOrderJob,
                           @Qualifier("expiredIpJob") Job expiredIpJob,
                           @Qualifier("expiredCertJob") Job expiredCertJob,
-                          @Qualifier("errorLogDailyJob") Job errorLogDailyJob
+                          @Qualifier("errorLogDailyJob") Job errorLogDailyJob,
+                          @Qualifier("errorLogMinuteJob") Job errorLogMinuteJob
                           ) {
         this.logService = logService;
         this.ipService = ipService;
@@ -50,19 +53,102 @@ public class BatchScheduler {
         this.expiredIpJob = expiredIpJob;
         this.expiredCertJob = expiredCertJob;
         this.errorLogDailyJob = errorLogDailyJob;
+        this.errorLogMinuteJob = errorLogMinuteJob;
+    }
+
+    /**
+     * sprig batch 실행 순서
+     * 1. Scheduler
+     * 2. JobLauncher
+     * 3. Job
+     * 4. Step
+     * 5. Reader
+     * 6. Processor
+     * 7. Writer
+     * 8. Listener
+     */
+
+    /**
+     * 기간이 만료된 IP 사용유무 N 처리
+     * 매일 오전 00시 05분
+     * expiredIpJob
+     */
+    @Scheduled(cron = "0 5 0 * * *")
+    public void expiredIpJob() {
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("expiredIpJob", System.currentTimeMillis())
+                    .toJobParameters();
+            JobExecution execution = jobLauncher.run(expiredIpJob, params);
+            log.info("expiredIpJob 실행 상태: {}", execution.getStatus());
+        } catch (Exception e) {
+            throw new JobException(e);
+        }
+    }
+
+    /**
+     * 기간이 만료된 인증서 사용유무 N 처리
+     * 매일 오전 00시 10분
+     * expiredCertJob
+     */
+    @Scheduled(cron = "0 10 0 * * *")
+    public void expiredCertJob() {
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("expiredCertJob", System.currentTimeMillis())
+                    .toJobParameters();
+            JobExecution execution = jobLauncher.run(expiredCertJob, params);
+            log.info("expiredCertJob 실행 상태: {}", execution.getStatus());
+        } catch (Exception e) {
+            throw new JobException(e);
+        }
+    }
+
+    /**
+     * 하루동안 에러로그를 조회해서 알림 발송
+     * 매일 오전 08시
+     * errorLogDailyJob
+     */
+//    //AS-IS
+//    @Scheduled(cron = "0 8 0 * * *")
+//    public void alarmErrorLogForDaily() {
+//        logService.getLogErrorForDaily();
+//    }
+    @Scheduled(cron = "0 0 8 * * *")
+    public void errorLogDailyJob() {
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("errorLogDailyJob", System.currentTimeMillis())
+                    .toJobParameters();
+            JobExecution execution = jobLauncher.run(errorLogDailyJob, params);
+            log.info("errorLogDailyJob 실행 상태: {}", execution.getStatus());
+        } catch (Exception e) {
+            throw new JobException(e);
+        }
     }
 
     /**
      * 10분마다 에러로그를 조회해서 5건 이상이면 알림 발송
-     * alarmErrorLogForTenMin
+     * 10분마다
+     * errorLogMinuteJob
      */
+//    @Scheduled(cron = "0 */10 * * * *")
+//    //@Scheduled(fixedRate = 600000)
+//    public void alarmErrorLogForTenMin() {
+//        logService.getLogErrorForTenMin();
+//    }
     @Scheduled(cron = "0 */10 * * * *")
-    //@Scheduled(fixedRate = 600000)
-    public void alarmErrorLogForTenMin() {
-        logService.getLogErrorForTenMin();
+    public void errorLogMinuteJob() {
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("errorLogMinuteJob", System.currentTimeMillis())
+                    .toJobParameters();
+            JobExecution execution = jobLauncher.run(errorLogMinuteJob, params);
+            log.info("errorLogMinuteJob 실행 상태: {}", execution.getStatus());
+        } catch (Exception e) {
+            throw new JobException(e);
+        }
     }
-
-
 
     /**
      * Ip 만료도래시 알림 발송
@@ -102,86 +188,6 @@ public class BatchScheduler {
     @Scheduled(cron = "0 0 8,12,16,18 * * *")
     public void alarmCertExpirationToManager() {
         certService.getCertPreExpirationToManager();
-    }
-
-    /**
-     * sprig batch 실행 순서
-     * 1. Scheduler
-     * 2. JobLauncher
-     * 3. Job
-     * 4. Step
-     * 5. Reader
-     * 6. Processor
-     * 7. Writer
-     * 8. Listener
-     */
-
-    /**
-     * 10분마다 에러로그를 조회해서 5건 이상이면 알림 발송
-     * alarmErrorLogForTenMin
-     */
-    @Scheduled(cron = "0 */10 * * * *")
-    public void errorLogTenMinJob() {
-        logService.getLogErrorForTenMin();
-    }
-
-    /**
-     * 하루동안 에러로그를 조회해서 알림 발송
-     * 매일 8시
-     * errorLogDailyJob
-     */
-//    //AS-IS
-//    @Scheduled(cron = "0 8 0 * * *")
-//    public void alarmErrorLogForDaily() {
-//        logService.getLogErrorForDaily();
-//    }
-    @Scheduled(cron = "0 8 0 * * *")
-    public void errorLogDailyJob() {
-        try {
-            JobParameters params = new JobParametersBuilder()
-                    .addLong("errorLogDailyJob", System.currentTimeMillis())
-                    .toJobParameters();
-            JobExecution execution = jobLauncher.run(errorLogDailyJob, params);
-            log.info("errorLogDailyJob 실행 상태: {}", execution.getStatus());
-        } catch (Exception e) {
-            throw new JobException(e);
-        }
-    }
-
-    /**
-     * 기간이 만료된 IP 사용유무 N 처리
-     * 매일 1시 5분
-     * expiredIpJob
-     */
-    @Scheduled(cron = "0 5 1 * * ?")
-    public void expiredIpJob() {
-        try {
-            JobParameters params = new JobParametersBuilder()
-                    .addLong("expiredIpJob", System.currentTimeMillis())
-                    .toJobParameters();
-            JobExecution execution = jobLauncher.run(expiredIpJob, params);
-            log.info("expiredIpJob 실행 상태: {}", execution.getStatus());
-        } catch (Exception e) {
-            throw new JobException(e);
-        }
-    }
-
-    /**
-     * 기간이 만료된 인증서 사용유무 N 처리
-     * 매일 1시 10분
-     * expiredCertJob
-     */
-    @Scheduled(cron = "30 0/1 * * * ?")
-    public void expiredCertJob() {
-        try {
-            JobParameters params = new JobParametersBuilder()
-                    .addLong("expiredCertJob", System.currentTimeMillis())
-                    .toJobParameters();
-            JobExecution execution = jobLauncher.run(expiredCertJob, params);
-            log.info("expiredCertJob 실행 상태: {}", execution.getStatus());
-        } catch (Exception e) {
-            throw new JobException(e);
-        }
     }
 
     // Person 배치는 매 분마다 실행 (cron: 매 분 0초에 실행)
