18628b67c3748716def8420dbf609908ab2ad206
Oh <osm723@gmail.com>
2025-07-16 15:16:54 +0900

    불용소스 제거

--- src/main/java/com/shds/sma/apps/ip/service/IpService.java
+++ src/main/java/com/shds/sma/apps/ip/service/IpService.java
@@ -1,13 +1,9 @@
 package com.shds.sma.apps.ip.service;
 
-import com.shds.sma.apps.admin.dto.member.MemberResponseDto;
-import com.shds.sma.apps.system.dto.SystemResponseDto;
 import com.shds.sma.apps.ip.dto.IpRequestDto;
 import com.shds.sma.apps.ip.dto.IpResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface IpService {
     Page<IpResponseDto> findIpByCond(IpRequestDto ipRequestDto, Pageable pageable);
